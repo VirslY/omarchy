f815b05e19e965c7f1784d6a01cb3c5c8d8a8825
VirslY <gergocraft11@gmail.com>
2025-08-30 19:00:52 +0200

    innitial
    omarchy without docker

---
+++ install/first-run/firewall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Allow nothing in, everything out
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+
+# Allow ports for LocalSend
+sudo ufw allow 53317/udp
+sudo ufw allow 53317/tcp
+
+# Allow SSH in
+sudo ufw allow 22/tcp
+
+# Turn on the firewall
+sudo ufw --force enable
+sudo ufw reload
